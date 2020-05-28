95f19581d6bd0f2ecd60fc73aa5415f66ba75f41
Claudio Antonius <antonius.claudio@gmail.com>
2020-05-29 00:07:23 +0700

    create react project + routing

---
+++ client/src/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { Home, Login, Register, MyFav, Detail } from './pages';
+import { Navbar } from './components';
+
+const routes = [
+  {
+    exact: true,
+    path: '/',
+    children: <Home/>
+  },
+  {
+    path: '/login',
+    children: <Login/>
+  },
+  {
+    path: '/register',
+    children: <Register/>
+  },
+  {
+    path: '/favorites',
+    children: <MyFav/>
+  },
+  {
+    path: '/detail',
+    children: <Detail/>
+  },
+];
+
+const AppRouter = () => (
+  <Switch>
+    {routes.map((route) => <Route key={route} {...route} />)}
+  </Switch>
+);
+
+function App() {
+  return (
+    <Router>
+        <Navbar />
+        <div>
+          <AppRouter />
+        </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ client/src/pages/MyFav.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function MyFav() {
+    return (
+        <h1>MyFav</h1>
+    )
+}
