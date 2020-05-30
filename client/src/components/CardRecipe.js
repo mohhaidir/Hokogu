6071880236ca9a05d0a4da4d75f51f9cf524c1af
Singgit Bramantha <bambadom@gmail.com>
2020-05-30 15:38:10 +0700

    sudah bisa menampilkan list favorite user

--- client/src/components/CardRecipe.js
+++ client/src/components/CardRecipe.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
 import { useStyles } from '../assets/css';
 import { Grid, Card, CardMedia, CardContent, CardActions, IconButton } from '@material-ui/core/';
 import { LocalDining, Favorite, AccessTime } from '@material-ui/icons';
 import { Link } from 'react-router-dom';
 
-export default function CardRecipe() {
+export default function CardRecipe(props) {
+    const [isFav,setIsFav] = useState(props.isFav)
     const classes = useStyles();
 
-    function addFavoritesAction () {
-        console.log('action add to fav')
+    function addFavoritesAction() {      
+        if(isFav){
+            axios({
+                method: "delete",
+                url: `${host}/favorites/${props.myFavorite.id}`,
+                headers: { token: localStorage.getItem('token') }
+            })
+                .then(response => {
+                    setBulkPosts(response.data)
+                    setPostLoading(false)
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        }
     }
-    
+
     return (
         <Grid item lg={3} sm={12}>
             <Link to='/detail' className='cardContent'>
@@ -18,21 +32,20 @@ export default function CardRecipe() {
                     <CardMedia
                         className={classes.media}
                         component="img"
-                        src='https://spoonacular.com/recipeImages/579247-556x370.jpg'
+                        src={props.myFavorite.image}
                         title="Nasi Goreng"
                     />
                     <CardContent className='titleCard'>
-                        <h2>Nasi Goreng</h2>
+                        <h2>{props.myFavorite.title}</h2>
                     </CardContent>
                     <CardActions disableSpacing className='bottomCard'>
                         <IconButton aria-label="add to favorites" onClick={addFavoritesAction}>
-                            {/* <Favorite className='iconColor'/> */}
-                            <Favorite />
+                        {isFav ? <Favorite className='iconColor' /> : <Favorite />}                 
                         </IconButton>
-                        <LocalDining/>
-                        2
-                        <AccessTime style={{marginLeft:'8px'}}/>
-                        25 mins
+                        {/* <LocalDining /> */}
+                        {props.myFavorite.serving}
+                        <AccessTime style={{ marginLeft: '8px' }} />
+                        {props.myFavorite.ready} mins
                     </CardActions>
                 </Card>
             </Link>
