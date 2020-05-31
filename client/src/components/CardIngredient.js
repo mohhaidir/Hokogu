9470c18c559400ad6c04b937a7db3e98f79b870f
Singgit Bramantha <bambadom@gmail.com>
2020-05-31 15:37:21 +0700

    gambar2 ingredient di halaman detail sudah keluar

--- client/src/components/CardIngredient.js
+++ client/src/components/CardIngredient.js
@@ -2,25 +2,25 @@ import React from 'react';
 import { Grid, CardActionArea, CardMedia, CardContent, Typography } from '@material-ui/core';
 import { useStyles } from '../assets/css';
 
-export default function CardIngredient() {
+export default function CardIngredient(props) {
     const classes = useStyles();
 
     return (
         <Grid item lg={2} md={3} xs={6}>
             <CardActionArea className='cardIngredient'>
                 <CardMedia
-                className={classes.media}
-                component="img"
-                src='https://spoonacular.com/cdn/ingredients_250x250/blueberries.jpg'
-                title="Bumbu"
+                    className={classes.media}
+                    component="img"
+                    src={`https://spoonacular.com/cdn/ingredients_250x250/${props.ingredient.image}`}
+                    title="Bumbu"
                 />
                 <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                    222.0 g
-                </Typography>
-                <Typography gutterBottom variant="h5" component="h2">
-                    blueberries
-                </Typography>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        {props.ingredient.amount.metric.value} {props.ingredient.amount.metric.unit}
+                    </Typography>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        {props.ingredient.name}
+                    </Typography>
                 </CardContent>
             </CardActionArea>
         </Grid>
--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,52 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react'
 import { Grid, Button, Box, Divider } from '@material-ui/core';
+import axios from 'axios'
 import { CardIngredient } from '../components';
 import { LocalDining, AccessTime, DoubleArrow } from '@material-ui/icons';
 import { useStyles } from '../assets/css';
+import { useParams, Link, useHistory } from 'react-router-dom'
 
 export default function Detail() {
+    const { id } = useParams()
     const classes = useStyles();
+    const [ingredients, setIngredients] = useState([])
+
+    useEffect(() => {
+        axios({
+            method: "get",
+            url: `https://api.spoonacular.com/recipes/${id}/ingredientWidget.json?apiKey=fd9f4d3bdf944da4add45901325004fe`
+        })
+            .then(response => {
+                console.log(response.data.ingredients)
+                setIngredients(response.data.ingredients)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }, [])
 
     return (
         <Box className='mainContent'>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
 
             <Grid container spacing={3} className='content'>
                 <Grid item xs={12}>
                     <h1>Nasi Goreng</h1>
                     <hr></hr>
-                    <Grid item xs={12} 
-                        container 
-                        direction='row' 
-                        spacing={3} 
-                        alignItems='center' 
+                    <Grid item xs={12}
+                        container
+                        direction='row'
+                        spacing={3}
+                        alignItems='center'
                         className='noMargin'
                     >
                         <Grid item lg={3} sm={12} className='imageFood'>
-                            <img src='https://spoonacular.com/recipeImages/579247-556x370.jpg' className='responsive'/>
+                            <img src='https://spoonacular.com/recipeImages/579247-556x370.jpg' className='responsive' />
                             <div className='detailImage'>
-                                <LocalDining className='iconColor'/>
+                                <LocalDining className='iconColor' />
                                 2
-                                <AccessTime className='iconColor' style={{marginLeft:'8px'}}/>
+                                <AccessTime className='iconColor' style={{ marginLeft: '8px' }} />
                                 25 mins
                             </div>
                         </Grid>
                         <Grid item lg={9} sm={12}>
                             <h1>Ingredients</h1>
-                            <Divider/>
-                            <Grid item container spacing={2} direction={'row'} style={{marginTop:'20px'}}>
-                                <CardIngredient/>
-                                <CardIngredient/>
-                                <CardIngredient/>
-                                <CardIngredient/>
-                                <CardIngredient/>
-                                <CardIngredient/>
-                                <CardIngredient/>
-                                <CardIngredient/>
+                            <Divider />
+                            <Grid item container spacing={2} direction={'row'} style={{ marginTop: '20px' }}>
+                                {ingredients.map((ingredient, idx) => {
+                                    return (
+                                        <CardIngredient key={idx} ingredient={ingredient} />
+                                    )
+                                })
+
+                                }
                             </Grid>
                         </Grid>
                         <Grid item xs={12} className='buttonCooking'>
