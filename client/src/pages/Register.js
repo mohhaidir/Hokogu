d73099ed1eb1dfc1cdec6badeef51d7b39210796
mohhaidir <khoiruljmbr@gmail.com>
2020-05-31 22:42:44 +0700

    button and state

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,198 +1,250 @@
-import React, {useState, useEffect} from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
+import React, { useState, useEffect } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
 // import FormControlLabel from '@material-ui/core/FormControlLabel';
 // import Checkbox from '@material-ui/core/Checkbox';
-import { Link, useHistory } from 'react-router-dom';
-import Paper from '@material-ui/core/Paper';
-import Box from '@material-ui/core/Box';
-import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles, useTheme, withStyles } from '@material-ui/core/styles';
+import { Link, useHistory } from "react-router-dom";
+import Paper from "@material-ui/core/Paper";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles, useTheme, withStyles } from "@material-ui/core/styles";
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
-import LargeGradientButton from '../components/LargeGardientButton'
-import {useDispatch, useSelector} from 'react-redux'
-import {register} from '../store/actions/userActions'
+import LargeGradientButton from "../components/LargeGardientButton";
+import { useDispatch, useSelector } from "react-redux";
+import { register } from "../store/actions/userActions";
+import CloudUploadIcon from "@material-ui/icons/CloudUpload";
 
-
-const ColorButton = withStyles((theme) => ({
-    root: {
-      color: theme.palette.getContrastText('#FF5F6D'),
-      backgroundColor: '#ff959c',
-      '&:hover': {
-        backgroundColor: '#FF5F6D',
-      },
-    },
-  }))(Button);
-  
-
-
-const useStyles = makeStyles((theme) => ({
-    margin: {
-        marginTop: '10px'
-      },
+const ColorButton = withStyles(theme => ({
   root: {
-    height: '92vh',
+    color: theme.palette.getContrastText("#FF5F6D"),
+    backgroundColor: "#ff959c",
+    "&:hover": {
+      backgroundColor: "#FF5F6D"
+    }
+  }
+}))(Button);
+
+const useStyles = makeStyles(theme => ({
+  margin: {
+    marginTop: "10px"
+  },
+  root: {
+    height: "92vh"
   },
   image: {
-    backgroundImage: 'url(https://www.ecopetit.cat/wpic/mpic/57-573449_sushi-wallpapers-download.jpg)',
-    backgroundRepeat: 'no-repeat',
+    backgroundImage:
+      "url(https://www.ecopetit.cat/wpic/mpic/57-573449_sushi-wallpapers-download.jpg)",
+    backgroundRepeat: "no-repeat",
     backgroundColor:
-      theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
+      theme.palette.type === "light"
+        ? theme.palette.grey[50]
+        : theme.palette.grey[900],
+    backgroundSize: "cover",
+    backgroundPosition: "center"
   },
   paper: {
     margin: theme.spacing(8, 4),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
   },
   avatar: {
     margin: theme.spacing(1),
-    backgroundColor: '#ff959c',
+    backgroundColor: "#ff959c"
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-    
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1)
   },
   submit: {
-    margin: theme.spacing(3, 0, 2),
+    margin: theme.spacing(3, 0, 2)
   },
+  input: {
+    display: "none"
+  }
 }));
 
 export default function Register() {
-    const history = useHistory()
-    const isLoggedIn = useSelector((state)=>state.userReducer.isLoggedIn)
-    const dispatch = useDispatch()
-    const classes = useStyles();
-    const [email, setEmail] = useState('');
-    const [name, setName] = useState('');
+  const history = useHistory();
+  const isLoggedIn = useSelector(state => state.userReducer.isLoggedIn);
+  const dispatch = useDispatch();
+  const classes = useStyles();
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [avatar, setAvatar] = useState(""); // <-- avatar
 
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
-    const nameHandle = (e) => {
-        setName(e.target.value)
+  const nameHandle = e => {
+    setName(e.target.value);
+  };
+
+  const emailHandle = e => {
+    setEmail(e.target.value);
+  };
+
+  const passwordHandle = e => {
+    setPassword(e.target.value);
+  };
+
+  const confirmPasswordHandle = e => {
+    setConfirmPassword(e.target.value);
+  };
+
+  const avatarHandle = e => {
+    setAvatar(e.target.value); // <-- avatar
+  };
+
+  const signUp = () => {
+    if (password === confirmPassword) {
+      let data = {
+        name: name,
+        email: email,
+        password: password,
+        avatar: "none"
+      };
+      dispatch(register(data));
     }
+  };
 
-    const emailHandle = (e) => {
-        setEmail(e.target.value)
+  useEffect(() => {
+    if (isLoggedIn) {
+      history.push(`/`);
     }
+  }, [isLoggedIn]);
 
-    const passwordHandle = (e) => {
-        setPassword(e.target.value)
-    }
+  return (
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid
+        style={{ backgroundColor: "white", boxShadow: "none" }}
+        item
+        xs={12}
+        sm={8}
+        md={5}
+        component={Paper}
+        elevation={6}
+        square
+      >
+        <br />
+        <br />
 
-    const confirmPasswordHandle = (e) => {
-        setConfirmPassword(e.target.value)
-    }
-    const signUp = () => {
-        if(password === confirmPassword){
-            let data = {
-                name: name,
-                email: email,
-                password: password,
-                avatar: 'none',
-            }
-            dispatch(register(data))
-        }
-    }
+        <div className={classes.paper}>
+          <h1 className="accountText">Create an account</h1>
 
-    useEffect(()=>{
-        if(isLoggedIn){
-            history.push(`/`)
-        }
-    }, [isLoggedIn])
+          <form className={classes.form} noValidate>
+            <TextField
+              color="secondary"
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="name"
+              label="Your Name"
+              name="name"
+              autoFocus
+              onChange={nameHandle}
+            />
 
-    return (
-        <Grid container component="main" className={classes.root}>
-        <CssBaseline/>
-        <Grid item xs={false} sm={4} md={7} className={classes.image} />
-        <Grid style={{  backgroundColor: 'white',boxShadow: 'none'}} item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-            <br/>
-            <br/>
+            <TextField
+              color="secondary"
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              onChange={emailHandle}
+            />
 
-            <div className={classes.paper}>
-                <h1 className='accountText'>
-                    Create an account
-                </h1>
+            <TextField
+              color="secondary"
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              onChange={passwordHandle}
+            />
 
-            <form className={classes.form} noValidate>
+            <TextField
+              color="secondary"
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="confirmpassword"
+              label="Confirm Password"
+              type="password"
+              id="confirmpassword"
+              onChange={confirmPasswordHandle}
+            />
 
-                <TextField
+            <TextField
+              color="secondary"
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              label="Avatar"
+              type="url"
+            />
+
+            <input
+              accept="image/*"
+              className={classes.input}
+              id="contained-button-file"
+              multiple
+              type="file"
+            />
+
+            <label htmlFor="contained-button-file">
+              <Button
+                variant="contained"
                 color="secondary"
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                id="name"
-                label="Your Name"
-                name="name"
-                autoFocus
-                onChange= {nameHandle}
-                />
+                size="small"
+                component="span"
+                className={classes.button}
+                startIcon={<CloudUploadIcon />}
+                onChange={avatarHandle}
+              >
+                Upload
+              </Button>
+            </label>
 
-                <TextField
-                color="secondary"
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                onChange= {emailHandle}
-                />
+            <ColorButton
+              onClick={signUp}
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.margin}
+            >
+              Register
+            </ColorButton>
 
-                <TextField
-                color="secondary"
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                onChange= {passwordHandle}
-
-                />
-                <TextField
-                color="secondary"
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                name="confirmpassword"
-                label="Confirm Password"
-                type="password"
-                id="confirmpassword"
-                onChange= {confirmPasswordHandle}
-
-                />
-
-                <ColorButton onClick={signUp} fullWidth variant="contained" color="primary" className={classes.margin}>
-                Register
-                </ColorButton>
-
-            
-                <Grid style={{marginTop: '30px'}}container>
-                <Grid item xs>
-                </Grid>
-                <Grid item>
-                <Link  to='/login'>
-                    <a style={{ color: '#FF5F6D', fontSize: '17px'}} href=''>Already Have an Account? Log In</a>
+            <Grid style={{ marginTop: "30px" }} container>
+              <Grid item xs></Grid>
+              <Grid item>
+                <Link to="/login">
+                  <a style={{ color: "#FF5F6D", fontSize: "17px" }} href="">
+                    Already Have an Account? Log In
+                  </a>
                 </Link>
-                </Grid>
-                </Grid>
-            </form>
-            </div>
-        </Grid>
-        </Grid>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+      </Grid>
+    </Grid>
   );
 }
