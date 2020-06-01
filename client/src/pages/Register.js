cff769b6b5193aa21e84f4d7b7d3fac74a4a5a66
mohhaidir <khoiruljmbr@gmail.com>
2020-06-01 21:08:05 +0700

    done

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -101,9 +101,9 @@ export default function Register() {
   const allInputs = { imgUrl: "" };
   const [imageAsFile, setImageAsFile] = useState("");
   const [imageAsUrl, setImageAsUrl] = useState(allInputs);
-  console.log(imageAsUrl);
+  // console.log(imageAsUrl);
   const handleImageAsFile = e => {
-    console.log(e.target.files, "----- files");
+    // console.log(e.target.files, "----- files");
     const image = e.target.files[0];
     setImageAsFile(imageFile => image);
     setPreview(image.name);
@@ -118,12 +118,12 @@ export default function Register() {
         avatar: "none"
       };
       if (imageAsFile === "") {
-        console.error(
-          `not an image, the image file is a ${typeof imageAsFile}`
-        );
+        // console.error(
+        //   `not an image, the image file is a ${typeof imageAsFile}`
+        // );
         data.avatar =
           "https://firebasestorage.googleapis.com/v0/b/hokugu…=media&token=a882876a-6e0b-4561-aa3a-81846b2c8b49";
-        console.log(data, "data avatar");
+        // console.log(data, "data avatar");
         dispatch(register(data));
       } else {
         const uploadTask = storage
@@ -156,8 +156,8 @@ export default function Register() {
                 data.avatar = fireBaseUrl;
                 // console.log(data.avatar, "----- ini avatar");
                 dispatch(register(data));
-                console.log(data, "---- data");
-                console.log(imageAsFile);
+                // console.log(data, "---- data");
+                // console.log(imageAsFile);
               });
           }
         );
