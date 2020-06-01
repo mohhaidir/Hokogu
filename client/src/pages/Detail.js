f13c1076057aef0758248face904f20fb865c606
Julio Sabandar <kjuliosabandar@gmail.comgit commit test.htmlgit push -u origin mastergit statusgit config --global user.email kjuliosabandar@gmail.com>
2020-06-01 23:04:22 +0700

    steps done

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -62,7 +62,6 @@ export default function Detail() {
                 <div style={{paddingLeft: '10vw', paddingRight: '10vw'}}>
                     <ExtendedFoodCard recipe={recipe} />
                     <br/>
-
                 </div>
                 </div>
             }
